c5c0be3232f4367146a1eaa7ce05cf2829576682
miuyongjun <1604165677@qq.com>
2016-06-09 22:45:08 +0800

    Initial commit

---
+++ app/src/main/java/miuyongjun/autils/MainActivity.java
@@ -0,0 +1,33 @@
+package miuyongjun.autils;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import miuyongjun.utillibrary.DoubleClickExitHelper;
+
+/**
+ * Created by miaoyongjun on 16/6/9.
+ * 　　　　    　┃┫┫　┃┫┫
+ * 　　　　    　┗┻┛　┗┻┛
+ */
+public class MainActivity extends AppCompatActivity {
+
+    DoubleClickExitHelper doubleClickExitHelper;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        doubleClickExitHelper = new DoubleClickExitHelper(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return doubleClickExitHelper.onKeyDown(keyCode, event);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
