758604780f19871edb80cc4c25208332430a3c58
miaoyongjun <1604165677@qq.com>
2017-07-21 13:53:55 +0800

    Update FileUtils.java

--- library/src/main/java/miaoyongjun/autil/utils/FileUtils.java
+++ library/src/main/java/miaoyongjun/autil/utils/FileUtils.java
@@ -46,7 +46,30 @@ import java.util.List;
 
 
 public class FileUtils {
-
+/**
+     * 得到SD卡根目录
+     */
+    public static File getRootPath() {
+        File path = null;
+        if (FileUtil.sdCardIsAvailable()) {
+            path = Environment.getExternalStorageDirectory(); // 取得sdcard文件路径
+        } else {
+            path = Environment.getDataDirectory();
+        }
+        return path;
+    }
+    
+     /**
+     * SD卡是否可用
+     */
+    public static boolean sdCardIsAvailable() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File sd = new File(Environment.getExternalStorageDirectory().getPath());
+            return sd.canWrite();
+        } else
+            return false;
+    }
+    
     /**
      * 根据文件路径获取文件
      *
